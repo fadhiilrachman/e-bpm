41f1dbf41c6f44a9b38de904a72304e9e8d83a72
Fadhiil Rachman <fadhiilrachman@gmail.com>
2021-12-15 04:00:18 +0700

    Init gatsby

---
+++ client/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "e-bpm",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
